48d0d211ff9164e25a8e714a3986d31003c8f711
Noah Pederson <pedersonnoah@gmail.com>
2015-07-01 18:03:44 -0500

    Some startup code

--- app/src/main/java/com/chiefnoah/mgareader/MainActivity.java
+++ app/src/main/java/com/chiefnoah/mgareader/MainActivity.java
@@ -1,16 +1,43 @@
 package com.chiefnoah.mgareader;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.chiefnoah.mgareader.Utils.Constants;
+
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences(Constants.GLOBAL_SHARED_PREFERENCES, MODE_MULTI_PROCESS);
+        //If it's the first run, show a dialog box to sign into a home server, or run in local mode
+        if(sharedPreferences.getBoolean(Constants.FIRST_RUN_KEY, true)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.startup_dialog_title);
+            builder.setPositiveButton(R.string.add_server, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //TODO: switch to add server fragment
+                }
+            });
+            builder.setNegativeButton(R.string.standalone_mode, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //TODO: switch to scanning fragment or something
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/chiefnoah/mgareader/Utils/Constants.java
@@ -0,0 +1,14 @@
+package com.chiefnoah.mgareader.Utils;
+
+/**
+ * Created by peder_000 on 7/1/2015.
+ */
+public class Constants {
+
+    //KEYS
+    public static final String GLOBAL_SHARED_PREFERENCES = "global shared preferences";
+
+
+    //Shared preferences keys
+    public static final String FIRST_RUN_KEY = "first run";
+}
